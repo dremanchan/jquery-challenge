c7a078c347474ecf17c554a6aa4ba329ac936056
dremanchan <85799261+dremanchan@users.noreply.github.com>
2021-12-09 12:52:31 -0600

    Update script.js
    appended h1 to dom

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 $(document).ready(onReady);
 
-function ready() {
+function onReady() {
     console.log('on ready');
+
+   $('h1').css("color", "red");
+
+   $('body').append(`
+   <h2>
+   Jquery can do a lot of things!
+   </h2>`)
+
 }
 
